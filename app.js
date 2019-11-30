26e0a8c30dfb68a9b0ba661e2e44ee90b42f1d0b
Omar <omar@omarsalempro.com>
2019-11-30 13:58:45 -0700

    Added express

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use( '/add-product',(req, res, next) =>  {
+    console.log('In the middleware');
+    res.send('<h1>The "Add Product" Page </h>');
+});
 
-const server = http.createServer(routes.handler);
+app.use( '/',(req, res, next) =>  {
+    console.log('In the middleware');
+    res.send('<h1>Hello from Express!!!</h>');
+});
 
-server.listen(3000);
+
+
+
+app.listen(8000);
 console.log("Server started at port 3000 ...");
