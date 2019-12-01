b2631f26e841155ddc5bf1ce332d6da564612cb4
Omar <omar@omarsalempro.com>
2019-12-01 13:13:30 -0700

    Added express routes

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use( '/add-product',(req, res, next) =>  {
-    console.log('In the middleware');
-    res.send('<h1>The "Add Product" Page </h>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use( '/',(req, res, next) =>  {
-    console.log('In the middleware');
-    res.send('<h1>Hello from Express!!!</h>');
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
-
-
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(8000);
-console.log("Server started at port 3000 ...");
+console.log("Server started at port 8000 ...");
